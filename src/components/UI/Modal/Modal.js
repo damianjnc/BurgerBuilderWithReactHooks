a56ed64867d765669ed37582d64660e2e096d494
damianjnc <damianjnc@gmail.com>
2019-07-24 06:00:02 +0100

    first commit

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import classes from './Modal.css';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
+
+class Model extends Component{
+
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+    }
+
+    componentWillUpdate(){
+        console.log('[Modal] WillUpdate');
+    }
+
+    render(){
+        return(
+            <Auxiliary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                    className={classes.Modal}>
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default Model;
